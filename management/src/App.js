14094da6611a8d89de6e0d576c965b19fc26ce2a
BAKHONGMIN <ghdals2655@naver.com>
2022-03-30 16:39:18 +0900

    고객정보 삽입

--- management/src/App.js
+++ management/src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className='gray-background'>
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers =[
+  {
+    'id':'ghdals2655',
+    'image' :'https://placeimg.com/64/64/1',
+    'name': "박홍민",
+    'birthday':"950108",
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':'ghdals2650',
+    'image' :'https://placeimg.com/64/64/2',
+    'name': "서연수",
+    'birthday':"950108",
+    'gender':'남자',
+    'job':'대학생'
+  },
+  {
+    'id':'ghdals2664',
+    'image' :'https://placeimg.com/64/64/3',
+    'name': "홍길동",
+    'birthday':"950108",
+    'gender':'남자',
+    'job':'대학생'
+  },
+]
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+              key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
